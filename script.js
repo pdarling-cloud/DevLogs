7b244d581e933f700cfaf5a5826b9c7ccf77ac07
pdarling-cloud <pdarling@icstars.org>
2025-10-17 18:46:00 -0500

    PenelopesDevLogs

---
+++ script.js
@@ -0,0 +1,20 @@
+let username = "Penelope";
+let number = "6";
+console.log(number );
+let numbers = 6;
+let x = number + numbers;
+console.log(x);
+let one = 1;
+let z = numbers+ one;
+console.log(z)
+
+let oneword = "Penelope";
+let twoword = "Darling";
+let cto = oneword + " " + twoword;
+console.log(cto);
+
+function add(a,b){
+let sum = a + b;
+console.log(sum);
+}
+add(33,22);
